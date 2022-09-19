dbd5adef64397099bd1b83b96cc27e4990b81544
Sol Cardozo <43156596+MVelasquez98@users.noreply.github.com>
2022-09-18 23:08:32 -0300

    midleware connecion a base de datos

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const path = require('path')
+const express = require('express'),
+path = require('path'),
+mysql = require('mysql'),
+myConnection = require('express-myconnection');
 const ejemploRoutes = require ('./src/routes/ejemploRoutes')
 
 const app = express()
@@ -7,7 +9,14 @@ app.set('port', 5000)
 
 //middlewares
 app.use(express.json()) //parsea las solicitudes al servidor
-
+app.use(myConnection(mysql, {
+    host: 'bclnro5zyhtbtwvryre1-mysql.services.clever-cloud.com',
+    user: 'ukxw0rjfbjdb9cfu',
+    password: 'CWZb3YYjm9MpSPeKXjXY',
+    port: 3306,
+    database: 'bclnro5zyhtbtwvryre1'
+  }, 'single'));
+  app.use(express.urlencoded({extended: false}));
 const logger = (req, res, next)=>{
     console.log(`Peticion ${req.method.toUpperCase()} recibida -  ${req.protocol}://${req.get('host')}${req.originalUrl}`)
     console.log("body: ", req.body)
