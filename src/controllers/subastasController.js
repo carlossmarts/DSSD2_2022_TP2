da235dea2cc8c14e315264f725a7ffde243849ea
Sol Cardozo <43156596+MVelasquez98@users.noreply.github.com>
2022-09-24 00:17:01 -0300

    subasta logica controller

--- src/controllers/subastasController.js
+++ src/controllers/subastasController.js
@@ -1,19 +1,70 @@
 const controller = {};
 controller.save = (req) => {
-    const data = req.body;
-    console.log(req.body)
-    req.getConnection((err, connection) => {
-      const query = connection.query('INSERT INTO subastas set ?', data, (err, subasta) => {
+  const data = req.body;
+  req.getConnection((err, conn) => {
+    conn.query(
+      "select idSubasta from productos where idProducto = ?",
+      [data.idProducto],
+      (err, rows) => {
         if (err) {
-            callback(err);
-            console.error("Error insertando subasta"+err.message);
-            return
-          }
-        return {
-            "statusCode":200,
-            "msg":"ok"
+          console.error("Error consultando subasta" + err.message);
+          return;
         }
-      })
-    })
-  };
+        const idSubasta = rows[0].idSubasta;
+        const subastaData = {
+          fechaPuja: data.fechaPuja,
+          idComprador: data.idComprador,
+          precioOfrecido: data.precioOfrecido,
+        };
+        if (idSubasta == null) {
+          conn.query(
+            "INSERT INTO subastas set ?",
+            subastaData,
+            (err, subasta) => {
+              if (err) {
+                console.error("Error insertando subasta" + err.message);
+                return connection.rollback(() => {
+                  throw err;
+                });
+              }
+              conn.query(
+                "UPDATE productos set idSubasta = ? where idProducto = ?",
+                [subasta.insertId, data.idProducto],
+                (err, rows) => {
+                  if (err) {
+                    console.error("Error actualizando producto" + err.message);
+                    return connection.rollback(() => {
+                      throw err;
+                    });
+                  }
+                  return {
+                    statusCode: 200,
+                    msg: "ok",
+                  };
+                }
+              );
+            }
+          );
+        } else {
+          conn.query(
+            "UPDATE subastas set ? where idSubasta = ?",
+            [subastaData, idSubasta],
+            (err, rows) => {
+              if (err) {
+                console.error("Error actualizando subasta" + err.message);
+                return connection.rollback(() => {
+                  throw err;
+                });
+              }
+              return {
+                statusCode: 200,
+                msg: "ok",
+              };
+            }
+          );
+        }
+      }
+    );
+  });
+};
 module.exports = controller;
